ad0efbefff312aa4fe4477d3a648e7ce099204af
Guilherme <guilhermeinfo1@gmail.com.br>
2023-01-27 09:47:00 -0300

    ASP.NET 5 - INTRODUCAO

---
+++ Backend/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : ControllerBase
+    {
+        [HttpGet]
+        public Atividade Get()
+        {
+            return new Atividade();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"primeiro get com id {id}";
+        }
+
+
+        [HttpPost]
+        public Atividade Post(Atividade atividade)
+        {
+            return atividade;
+        }
+
+
+    }
+}
